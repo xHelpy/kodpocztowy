a7ed71f92161534856d521f50a8afcbae0b7a9fc
xHelpy <xiozeerpl@gmail.com>
2024-11-07 09:23:28 +0100

    adding files

---
+++ kodpocztowy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kodpocztowy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ kodpocztowy/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.Maui.Graphics.Text;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace kodpocztowy
+{
+    public partial class MainPage : ContentPage
+    {
+        public class postCode
+        {
+            [JsonPropertyName("post code")]
+            public string? post_code { get; set; }
+            public string? country { get; set; }
+            [JsonPropertyName("country abbreviation")]
+            public string? country_abbreviation { get; set; }
+            public IList<Place> places { get; set; }
+        }
+        public class Place
+        {
+            [JsonPropertyName("place name")]
+            public string place_name { get; set; }
+            public string? longitude { get; set; }
+            public string? state { get; set; }
+            [JsonPropertyName("state abbreviation")]
+            public string? state_abbreviation { get; set; }
+            public string? latitude { get; set; }   
+        }
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string json;
+            string url = "https://api.zippopotam.us/us/90210";
+            using (var webClient = new WebClient())
+            {
+                json = webClient.DownloadString(url);
+            }
+            postCode data = JsonSerializer.Deserialize<postCode>(json);
+
+
+            Test.Text = data.places[0].place_name;
+        }
+    }
+
+}
